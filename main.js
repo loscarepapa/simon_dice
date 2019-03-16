bc4b8b7bad77d7ce54487a2dffd61cd660acd57b
loscarepapa <loscarepapap@gmail.com>
2019-03-16 12:20:31 -0600

    0 de la alternativa junto con las variables globales de el circulo

---
+++ main.js
@@ -0,0 +1,11 @@
+var verde,
+    azul,
+    amarillo,
+    rojo;
+
+setTimeout(() => {
+    verde = document.getElementById("Verde"),
+    azul = document.getElementById("Azul"),
+    amarillo = document.getElementById("Amarillo"),
+    rojo = document.getElementById("Rojo");
+}, 0);
