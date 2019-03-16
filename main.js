ad5c4b24255cc44bf69e8965084d30334da91813
loscarepapa <loscarepapap@gmail.com>
2019-03-16 16:46:27 -0600

    Creacion del primer nivel y guardar los datos en la varible

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var verde,
     azul,
     amarillo,
-    rojo;
+    rojo,
+    random,
+    serie = [],
+    numColores = 0;
 
 setTimeout(() => {
     verde = document.getElementById("Verde"),
@@ -9,3 +12,40 @@ setTimeout(() => {
     amarillo = document.getElementById("Amarillo"),
     rojo = document.getElementById("Rojo");
 }, 0);
+
+function iniciar(id){
+    document.getElementById(id).style.display = "none";
+    setTimeout(() => {
+        
+        random = Math.round(Math.random() * 3)
+        serie[numColores] = random;
+        numColores++;
+
+        if(random == 0){
+            verde.style.border = "3px solid #fff";
+            setTimeout(() => {
+            verde.style.border = "";                
+            }, 1000);
+        } else if (random == 1) {
+            azul.style.border = "3px solid #fff";
+            setTimeout(() => {
+            azul.style.border = "";                
+            }, 1000);
+        } else if (random == 2) {
+            amarillo.style.border = "3px solid #fff";
+            setTimeout(() => {
+            amarillo.style.border = "";                
+            }, 1000);
+        } else if (random == 3) {
+            rojo.style.border = "3px solid #fff";
+            setTimeout(() => {
+            rojo.style.border = "";                
+            }, 1000);
+        }
+        
+        console.log(random)
+        console.log(serie)
+
+
+    }, 1000);
+}
