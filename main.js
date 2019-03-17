255f163b93f07bdd4de20eb1ae6e49c8ff2fece9
loscarepapa <loscarepapap@gmail.com>
2019-03-17 07:10:58 -0600

    Creacion de la pantalla de bloqueo

--- main.js
+++ main.js
@@ -17,11 +17,15 @@ setTimeout(() => {
 
 function iniciar(id){
     document.getElementById(id).style.display = "none";
+    document.getElementById("bloqueo").className = "bloqueo";
     setTimeout(() => {
         
         random = Math.round(Math.random() * 3)
         serie[numColores] = random;
         numColores++;
+        setTimeout(() => {
+            document.getElementById("bloqueo").className = "none";
+        }, 1000);
 
         if(random == 0){
             verde.style.border = "3px solid #fff";
@@ -66,6 +70,9 @@ function prueba(id){
     if (id == serie[numColores - 1]) {
         document.getElementById("puntos_Totales").innerHTML = nivel + 1;
         nivel++;
+        setTimeout(() => {
+            sigNivel();
+        }, 1000);
     }else{
         document.getElementById("iniciar").style.display = "block";
         document.getElementById("puntos_Totales").innerHTML = "";
@@ -75,4 +82,31 @@ function prueba(id){
         numColores = 0;
     }
 
+}
+function sigNivel() {
+    random = Math.round(Math.random() * 3)
+    serie[numColores] = random;
+    numColores++;
+
+    if (random == 0) {
+        verde.style.border = "3px solid #fff";
+        setTimeout(() => {
+            verde.style.border = "";
+        }, 1000);
+    } else if (random == 1) {
+        azul.style.border = "3px solid #fff";
+        setTimeout(() => {
+            azul.style.border = "";
+        }, 1000);
+    } else if (random == 2) {
+        amarillo.style.border = "3px solid #fff";
+        setTimeout(() => {
+            amarillo.style.border = "";
+        }, 1000);
+    } else if (random == 3) {
+        rojo.style.border = "3px solid #fff";
+        setTimeout(() => {
+            rojo.style.border = "";
+        }, 1000);
+    }
 }
