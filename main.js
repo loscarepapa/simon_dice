1b457a996a9af816146a865262a519a6aef150ee
loscarepapa <loscarepapap@gmail.com>
2019-03-17 18:55:54 -0600

    Creacion de el array de prueba

---
+++ main.js
@@ -0,0 +1,17 @@
+let verde,
+    azul,
+    amarillo,
+    rojo
+    serie = [0,2,3,1,0,2,0,1,3,2,2,1,0,3]
+    numSerie = serie.length;
+
+    setTimeout(() => {
+        verde = document.getElementById("Verde");
+        azul = document.getElementById("Azul");
+        amarillo = document.getElementById("Amarillo");
+        rojo = document.getElementById("Rojo");
+    }, 0);
+
+    function pregunta(num) {
+        
+    }
